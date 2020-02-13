d63522328bb0ed4ac19b3508cc68855f6189d05d
Md. A R Joy <email2arjoy@gmail.com>
2020-02-13 17:59:31 +0600

    Add files via upload

--- ng.js
+++ ng.js
@@ -1,6 +1,54 @@
 angular.module("myApp", ["ngMaterial"])
-.controller("myCtrl", jyoti);
+.controller("myCtrl", formFeed);
 
-function jyoti($scope){
+function formFeed($scope){
+   $scope.profileData = [];
 
+   $scope.info = {
+         "name" : "",
+         "contact" : {
+            "phone" : "",
+            "email" : "",
+         },
+         "address" : {
+            "parmanent" : {
+               "house" : "",
+               "road" : "",
+               "district" : "",
+            },
+            "present" : {
+               "block" : "",
+               "avenue" : "",
+            },
+         }
+      };
+
+
+   $scope.submit = submit;
+
+
+
+   function submit() {
+      // $scope.list.push(this.text);
+      //     $scope.text = '';
+      $scope.profileData.push($scope.info);
+          $scope.info = {
+         "name" : "",
+          "contact" : {
+             "phone" : "",
+             "email" : "",
+          },
+          "address" : {
+             "parmanent" : {
+                "house" : "",
+                "road" : "",
+                "district" : "",
+             },
+             "present" : {
+                "block" : "",
+                "avenue" : "",
+             },
+          }
+       };
+   }
 };
